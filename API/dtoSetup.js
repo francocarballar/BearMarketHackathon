3673cce79ce15a2d4f111dcd7ecb21f8048cc6e8
Sebastian Lujan <glujan.recalde@gmail.com>
2022-09-19 02:32:47 -0300

    Add API, ConsumerApi, model, Setup

---
+++ API/dtoSetup.js
@@ -0,0 +1,10 @@
+//const getGameResolve0 = useContractRead({
+const dto = {
+    chain: 0,
+    addressOrName: '',
+    contractInterface: '',
+    functionName: '',
+    args: [],
+}
+
+export default dto
