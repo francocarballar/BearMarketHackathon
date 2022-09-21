adeaf161fe2970b30c40f36da50a4cf30794bbb0
francocarballar <francocarballar@gmail.com>
2022-09-21 08:32:17 -0300

    Borre la configuración del archivo next.config.js y cree una nueva

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
   trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
   exportPathMap: function () {
     return {
-      '/': { page: '/index' },
-      '/results': { page: '/results' },
-      '/signin': { page: '/signin' },
-      '/user': { page: '/user' }
+      '/': { page: '/' }
     }
   }
-}
-
-module.exports = {
-  nextConfig
 }
