4c857975b40a51fb613813f115c560b973e41e9c
francocarballar <francocarballar@gmail.com>
2022-09-21 12:26:42 -0300

    Arreglé el tema del camelCase en los atributos del head de la página _app.js y _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -74,10 +74,10 @@ function MyApp ({ Component, pageProps }) {
                 <link
                   rel='icon'
                   href='/icon/logo_icon.ico'
-                  onload="this.rel='stylesheet'"
+                  onLoad="this.rel='stylesheet'"
                 />
-                <meta charset='UTF-8' />
-                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+                <meta charSet='UTF-8' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
                 <meta
                   name='viewport'
                   content='width=device-width, initial-scale=1.0'
--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,14 @@ import { CssBaseline } from '@nextui-org/react'
 
 export default function Document () {
   return (
-    <Html lang='es'>
+    <Html lang='en'>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap'
           rel='stylesheet'
