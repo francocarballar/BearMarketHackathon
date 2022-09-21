7d138fe13992876f7107ab39c5e1b351ea9d28d3
francocarballar <francocarballar@gmail.com>
2022-09-21 07:42:52 -0300

    Agregué next export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return { '/': { page: '/' } }
+  }
 }
 
 module.exports = {
-  nextConfig,
-  exportTrailingSlash: true,
-  exportPathMap: function () {
-    return { '/': { page: '/' } }
-  }
+  nextConfig
 }
