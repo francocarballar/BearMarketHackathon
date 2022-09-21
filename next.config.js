82e8d9f93a237321b5db8a80248c53beb461b8aa
francocarballar <francocarballar@gmail.com>
2022-09-21 07:59:23 -0300

    Deshabilite la optimización de imagenes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
